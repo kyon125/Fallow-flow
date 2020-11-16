4047767e5e4c2277a2b2452621c1ce95edbbe1fb
Kyon_dreamer <dream7652210@gmail.com>
2020-11-16 17:36:29 +0800

    11/16

---
+++ Assets/Scripts/stage1/sence2/cine_change.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class cine_change : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cine;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            cine.SetActive(false);
+        }        
+    }
+}
