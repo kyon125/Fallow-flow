f99258130c01cda908a122c4137a7de0e3b349f2
Kyon_dreamer <dream7652210@gmail.com>
2020-11-20 01:36:42 +0800

    俄羅斯方塊雛形

---
+++ Assets/Scripts/new_stage/playerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class playerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        red2_control();
+    }
+    void red2_control()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.DOBlendableLocalMoveBy(Vector3.right, 0.5f).SetEase(Ease.OutQuad);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.DOBlendableLocalMoveBy(Vector3.left, 0.5f).SetEase(Ease.OutQuad);
+        }
+    }
+    public enum playermove
+    {
+        red1,
+        red2,
+        inpass
+    }
+}
---
+++ Assets/Scripts/test_c#/trtrisMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class trtrisMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    public float pos , time;
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+        transform.DOMoveY(pos, time).SetEase(Ease.Linear);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
