8e37070502f40e69da2912487b687803ead17750
Kyon_dreamer <dream7652210@gmail.com>
2020-12-02 10:46:01 +0800

    mainmenu最高分完成

---
+++ Assets/Scripts/mainmeunscore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mainmeunscore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text s1, s2, s3;
+    void Start()
+    {
+        s1.text = "BEST SCORE:" + PlayerPrefs.GetInt("s1_Score");
+        s2.text = "BEST SCORE:" + PlayerPrefs.GetInt("s2_Score");
+        s3.text = "BEST SCORE:" + PlayerPrefs.GetInt("s3_Score");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
